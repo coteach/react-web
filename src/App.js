24690468a8817cce5c073c893a09001058942b88
Nesl amd <neslxzhen@gmail.com>
2021-03-06 17:27:56 +0800

    feat: like google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import TitleBar from './components/TitleBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+
+
+			<header>
+
+			</header>
+
+
+			<div className="outer">
+				<div className="session">
+					<TitleBar></TitleBar>
+				</div>
+				
+			</div>
+
+
+			<div className="footer">
+
+			</div>
+
+
+		</div>
+	);
 }
 
 export default App;
